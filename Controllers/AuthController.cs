e77bb723d66b15aea32d7e6a4b45477fd95ae2ee
kamoliddin <kamoliddinzaylobiddinov677@gmail.com>
2024-01-22 04:49:33 -0800

    Register and Login Added

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using BooksWepAPiDtos.UserDto;
+using BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWebAPi_New_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserDto dto)
+        {
+            var result = await _userService.RegisterUserAsync(dto);
+            if (result.IsSuccessed)
+            {
+                return Ok("User Created");
+            } 
+            return BadRequest(result.ErrorrMessages); 
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginUserDto loginUserDto)
+        {
+            var result = await _userService.LoginUserAsync(loginUserDto);
+            if (result.IsSuccessed)
+            {
+                return Ok("Successfully logged in !");
+            }
+            return BadRequest(result.ErrorrMessages);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using BusinessLogicLayer.Interfaces;
 using BusinessLogicLayer.Services;
 using DataAccessLayer.Data;
 using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
 using DataAccessLayer.Repasitories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,14 +29,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AuthDbContext>(opt =>
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
+
 
 builder.Services.AddTransient<ICategoryInterface, CategoryRepasitory>();
 builder.Services.AddTransient<IBookInterface, BookRepasitory>();
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<IBookService, BookService>();
 builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 #region
 var mapperConfig = new MapperConfiguration(mc =>
